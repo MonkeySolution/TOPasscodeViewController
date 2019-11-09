9d69568fddb143e657b76cdf39b0b6f75fd9403a
monkeysolution <info@monkeysolution.com>
2019-11-08 19:48:40 -0800

    Done

--- TOPasscodeViewController/TOPasscodeViewController.h
+++ TOPasscodeViewController/TOPasscodeViewController.h
@@ -23,7 +23,7 @@
 #import <UIKit/UIKit.h>
 #import "TOPasscodeViewControllerConstants.h"
 #import "TOPasscodeView.h"
-#import "Models/PresentationStrings.h"
+#import "PresentationStrings.h"
 
 
 NS_ASSUME_NONNULL_BEGIN
